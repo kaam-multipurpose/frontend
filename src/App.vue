a12c8696551e11ac0726ad4cfb4212900873143a
Habeeb Shittu <habeeb4shittu@gmail.com>
2025-09-24 18:23:35 +0100

    Completed:Login page design

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,7 @@ import {RouterLink, RouterView} from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/images/logo.svg" width="125" height="125"/>
-
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  
   <RouterView/>
 
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import LoginPage from "@/views/pages/LoginPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
     },
+    {
+      path: "/login",
+      name: "login",
+      component: LoginPage,
+    },
   ],
-})
+});
 
-export default router
+export default router;
