9b7da5766104548ab22a43ffb681d5c588f3953b
Habeeb Shittu <habeeb4shittu@gmail.com>
2026-02-04 11:33:03 +0100

    Category features completion

--- src/config/sidebar-menu.config.ts
+++ src/config/sidebar-menu.config.ts
@@ -42,7 +42,7 @@ export const sidebarMenuConfig: SidebarMenuConfigType[] = [
   {
     name: "Categories",
     icon: Boxes,
-    path: "",
+    path: "/app/categories",
   },
   {
     name: "Products",
